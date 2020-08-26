08dd017519f2c8d4184521ec6eaa80b97f52c5ba
Shreya mittal <64747840+Shreya-cell@users.noreply.github.com>
2020-08-26 16:47:31 +0530

    Create  NullStudentObjectException.java

---
+++ com.mile1.exception/NullStudentObjectException.java
@@ -0,0 +1,7 @@
+package com.mile1.exception;
+/*------class for null student object exception-------*/
+public class NullStudentObjectException extends Exception {
+	public String toString() {
+		return "object is null";
+	}
+}
