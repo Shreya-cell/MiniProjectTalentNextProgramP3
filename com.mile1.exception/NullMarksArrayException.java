f2b446544a1fac18526d9fb302783f6fa5b2ce29
Shreya mittal <64747840+Shreya-cell@users.noreply.github.com>
2020-08-26 16:46:34 +0530

    Create  NullMarksArrayException.java

---
+++ com.mile1.exception/NullMarksArrayException.java
@@ -0,0 +1,8 @@
+package com.mile1.exception;
+
+/*------class for null marks array exception-------*/
+public class NullMarksArrayException extends Exception{
+		public String toString() {		//override toString() method
+			return "marks array is null";
+		}
+}
